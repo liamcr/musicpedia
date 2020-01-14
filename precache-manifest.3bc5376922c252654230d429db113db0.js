a3022fc17c3882e8ec36b807091f821db365281f
Liam Crocket <liamcrocket@gmail.com>
2020-01-14 16:30:24 -0500

    Updates

---
+++ precache-manifest.3bc5376922c252654230d429db113db0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf7c8b89125cc0ece3d9414bc07738d4",
+    "url": "/musipedia/index.html"
+  },
+  {
+    "revision": "9b68d15a4b8c9563a0c2",
+    "url": "/musipedia/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7608301533cd17930202",
+    "url": "/musipedia/static/js/2.15954188.chunk.js"
+  },
+  {
+    "revision": "b4388488e6778855973fb4c6caca93fc",
+    "url": "/musipedia/static/js/2.15954188.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b68d15a4b8c9563a0c2",
+    "url": "/musipedia/static/js/main.91a7df50.chunk.js"
+  },
+  {
+    "revision": "b968ed3b33123d413336",
+    "url": "/musipedia/static/js/runtime-main.8df0e77c.js"
+  }
+]);
